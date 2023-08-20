1181f94a924493c1fb4498a63972bf259b9fd3d8
Pavel Bryzgalov <pavelbryz@gmail.com>
2023-08-20 11:49:43 +0930

    lots2

--- AccessibleTourism/main_page/views.py
+++ AccessibleTourism/main_page/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
 from .models import Place
 
-from dotenv import load_dotenv
 import pyttsx3
 # Create your views here.
 
